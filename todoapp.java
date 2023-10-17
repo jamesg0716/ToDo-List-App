9a3ecb5d1ea6c6b6bd37097f09e241cc108a6e5f
James Glass <jamesglass774@gmail.com>
2023-10-16 21:13:31 -0500

    Added a remove button that allows users to remove completed or unwanted tasks.

--- todoapp.java
+++ todoapp.java
@@ -6,26 +6,27 @@ import java.awt.event.ActionListener;
 class ToDoApp {
     public static void main(String[] args) {
         JPanel panel = new JPanel(new BorderLayout());
-        JButton b = new JButton("Add a Task");
-        b.setBounds(250,650,100, 40);
+        JButton addButton = new JButton("Add a Task");
+        JButton removeButton = new JButton("Remove Task");
 
         DefaultListModel<String> listModel = new DefaultListModel<>();
 
         JList<String> list1 = new JList<>(listModel);
 
         JScrollPane scrollPane = new JScrollPane(list1);
-
+        list1.setFixedCellHeight(25);
         list1.setLayoutOrientation(JList.VERTICAL);
         panel.add(scrollPane, BorderLayout.CENTER);
 
         JFrame f = new JFrame("ToDo List");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(400, 600);
-        f.add(b, BorderLayout.SOUTH);
+        f.add(addButton, BorderLayout.NORTH);
+        f.add(removeButton, BorderLayout.SOUTH);
         f.add(panel, BorderLayout.CENTER);
         f.setVisible(true);
 
-        b.addActionListener(new ActionListener() {
+        addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String newTask = JOptionPane.showInputDialog("Enter a task: ");
@@ -34,5 +35,14 @@ class ToDoApp {
                 }
             }
         });
+
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = list1.getSelectedIndex();
+                listModel.removeElementAt(index);
+            }
+        }); 
+
     }
 }
